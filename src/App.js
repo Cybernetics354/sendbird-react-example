d19852bc7d0333ef05aae6b8443b8750412ea888
Hanif Mifta <hanif@jakpat.net>
2022-07-05 15:44:03 +0700

    chore: initial scaffold

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { App as SendBirdApp } from "sendbird-uikit";
+import "sendbird-uikit/dist/index.css";
 
 function App() {
+  const appID = "7FE9002E-FFA4-4221-8910-1853B799B934";
+  const accessToken = "0dcefdae1ee72e9a8cf3349adcde49bc946fe2c4";
+  const [id, setID] = useState("");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const _id = params.get("id");
+
+    setID(_id);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SendBirdApp appId={appID} userId={id} accessToken={accessToken} />
     </div>
   );
 }
