6d6cecbf8c2d67d1552ae751c19d55d2a8479476
Hanif Mifta <hanif@jakpat.net>
2022-07-08 10:22:37 +0700

    Add access token input

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -5,14 +5,21 @@ import "./dashboard.scss";
 
 export default function Dashboard() {
   const appID = process.env.REACT_APP_SENDBIRD_APP_ID;
-  const accessToken = process.env.REACT_APP_SENDBIRD_ACCESS_TOKEN;
+  const defaultAccessToken = process.env.REACT_APP_SENDBIRD_ACCESS_TOKEN;
+
   const [id, setID] = useState("");
+  const [accessToken, setAccessToken] = useState(defaultAccessToken);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    const _id = params.get("id");
 
+    const _id = params.get("id");
     setID(_id);
+    
+    const _accessToken = params.get("accessToken");
+    if(_accessToken) {
+      setAccessToken(_accessToken);
+    }
   }, []);
 
   return (
